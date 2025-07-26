0434e8d34ff14eebe6d7e7484897922daaa9429e
Sahil Paul <paulcode1234@gmail.com>
2025-07-27 04:28:33 +0530

    spinner in ad and edit button

--- src/dynamicComponents/AddNote.js
+++ src/dynamicComponents/AddNote.js
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useState } from 'react';
 import NoteContext from '../contexts/NoteContext';
 import DisableButtonContext from '../contexts/DisableButtonContext';
 import AlertContext from '../contexts/AlertContext';
-import {capitalize} from '../Functions';
+import { capitalize } from '../Functions';
 
 export default function AddNote() {
   // Destructing context values passed from the parent
@@ -17,7 +17,7 @@ export default function AddNote() {
     tags: ""
   });
 
-//--------------------------------------------- Logic to handle on change event of the form ---------------------------------------------\\
+  //--------------------------------------------- Logic to handle on change event of the form ---------------------------------------------\\
 
   // Function to handle on change event
   const handleOnChange = (e) => {
@@ -26,26 +26,27 @@ export default function AddNote() {
       [e.target.name]: e.target.value
     }))
   };
-//-------------------------------------------------------************-------------------------------------------------------\\
+  //-------------------------------------------------------************-------------------------------------------------------\\
 
 
-//--------------------------------------------- Logic to handle clicking add button ---------------------------------------------\\
+  // State to check  API response delivered
+  const [addApiCall, setAddApiCall] = useState(false)
+  // State to check  API response delivered
+  const [editApiCall, setEditApiCall] = useState(false)
+
+
+  //--------------------------------------------- Logic to handle clicking add button ---------------------------------------------\\
 
   // Function to handle on click event for adding note
   const handleOnAddClick = async (e) => {
     // Prevent the default action of reloading the page
     e.preventDefault();
 
+    setAddApiCall(true);// Change the state once API called
     let response = null;
     try {
       response = await addNote(note.title, note.description, capitalize(note.tags));
-      // Check if user input has tags
-      // if (note.tags==="") {
-      //   response = await addNote(note.title, note.description);
-      // }
-      // else {
-      //   response = await addNote(note.title, note.description, capitalize(note.tags));
-      // }
+      setAddApiCall(false);// Change the state once response is delivered
       // Check API response
       if (!response.success) {
         console.error(response); // Capture response errors
@@ -69,10 +70,10 @@ export default function AddNote() {
       })
     }
   };
-//-------------------------------------------------------************-------------------------------------------------------\\
+  //-------------------------------------------------------************-------------------------------------------------------\\
 
 
-//--------------------------------------------- Logic to handle clicking edit button ---------------------------------------------\\
+  //--------------------------------------------- Logic to handle clicking edit button ---------------------------------------------\\
 
   // UseEffect to manage the form population during edit note
   useEffect(() => {
@@ -89,16 +90,12 @@ export default function AddNote() {
   const handleOnEditClick = async (e) => {
     // Prevent the default action of reloading the page
     e.preventDefault();
+
+    setEditApiCall(true);// Change the state once API called
     let response = null;
     try {
       response = await editNote(disableButton.editNote._id, note.title, note.description, capitalize(note.tags));
-      // Check if user input has tags
-      // if (note.tags==="") {
-      //   response = await editNote(disableButton.editNote._id, note.title, note.description);
-      // }
-      // else {
-      //   response = await editNote(disableButton.editNote._id, note.title, note.description, capitalize(note.tags));
-      // }
+      setEditApiCall(false);// Change the state once response is delivered
       // Check API response
       if (!response.success) {
         console.error(response); // Capture response errors
@@ -131,54 +128,54 @@ export default function AddNote() {
       })
     }
   };
-//-------------------------------------------------------************-------------------------------------------------------\\
+  //-------------------------------------------------------************-------------------------------------------------------\\
 
 
-//------------------------------------- Logic for text annimation for form placeholders -------------------------------------\\
+  //------------------------------------- Logic for text annimation for form placeholders -------------------------------------\\
 
   // State to to store the animated text for the form placeholders
-      const [animationText, setanimationText] = useState({
-          titleText: "",
-          descriptionText: ""
-      });
-      // UseEffect to manage the animation of the text in form placeholders
-      useEffect(() => {
-          // Function to animate the text string
-          const animateText = (text, type, interval) => {
-              let i = 0;              // Set counter to manage the setinterval
-              let displayText = ""    // Set dummy text for smoother assignment of state variable
-              // Set the setInterval to animate the text at equal intervals
-              const startAnimation = () => {
-                  const intervalId = setInterval(() => {
-                      if (i < text.length) {
-                          if (text[i] !== undefined) {
-                              displayText += text[i];
-                              setanimationText(prev => ({...prev, [type]:displayText}));
-                          }
-                          // console.log(displayText);
-                          i++;
-                      } else {
-                          clearInterval(intervalId);
-                          setanimationText(prev => ({...prev, [type]:displayText}));  // Clear the text
-                          i = 0;                  // Reset counter
-                          displayText = "";       // Reset dummy text
-                          // Reset and restart after 1s
-                          setTimeout(() => {
-                              startAnimation();       // Restart animation
-                          }, 1000);
-                      }
-                  }, interval);
-              };
-              startAnimation(); // start when component mounts
+  const [animationText, setanimationText] = useState({
+    titleText: "",
+    descriptionText: ""
+  });
+  // UseEffect to manage the animation of the text in form placeholders
+  useEffect(() => {
+    // Function to animate the text string
+    const animateText = (text, type, interval) => {
+      let i = 0;              // Set counter to manage the setinterval
+      let displayText = ""    // Set dummy text for smoother assignment of state variable
+      // Set the setInterval to animate the text at equal intervals
+      const startAnimation = () => {
+        const intervalId = setInterval(() => {
+          if (i < text.length) {
+            if (text[i] !== undefined) {
+              displayText += text[i];
+              setanimationText(prev => ({ ...prev, [type]: displayText }));
+            }
+            // console.log(displayText);
+            i++;
+          } else {
+            clearInterval(intervalId);
+            setanimationText(prev => ({ ...prev, [type]: displayText }));  // Clear the text
+            i = 0;                  // Reset counter
+            displayText = "";       // Reset dummy text
+            // Reset and restart after 1s
+            setTimeout(() => {
+              startAnimation();       // Restart animation
+            }, 1000);
           }
-          const titleText = "(min 3 characters....)";
-          const descriptionText = "(min 5 characters....)";
-          animateText(titleText, "titleText", 200);
-          animateText(descriptionText, "descriptionText", 200);
-          // Optional cleanup
-          return () => clearInterval(); // cleanup in case component unmounts
-      }, []);
-//------------------------------------- ************************************* -------------------------------------||
+        }, interval);
+      };
+      startAnimation(); // start when component mounts
+    }
+    const titleText = "(min 3 characters....)";
+    const descriptionText = "(min 5 characters....)";
+    animateText(titleText, "titleText", 200);
+    animateText(descriptionText, "descriptionText", 200);
+    // Optional cleanup
+    return () => clearInterval(); // cleanup in case component unmounts
+  }, []);
+  //------------------------------------- ************************************* -------------------------------------||
 
   return (
     <div className="container mb-5">
@@ -201,8 +198,28 @@ export default function AddNote() {
           <label htmlFor="tags" className="form-label">Tags</label>
           <input type="text" className="form-control" id="tags" name="tags" placeholder="Enter notes tags" value={note.tags} onChange={handleOnChange} />
         </div>
-        <button type="submit" className="btn btn-outline-success" disabled={disableButton.addButton || note.title.trim().length < 3 || note.description.trim().length < 5 ? true : false} onClick={handleOnAddClick}>Add Note</button>
-        <button type="submit" className="btn btn-outline-success mx-3" disabled={disableButton.editButton || note.title.trim().length < 3 || note.description.trim().length < 5 ? true : false} onClick={handleOnEditClick}>Edit Note</button>
+        <div className="d-flex align-items-center">
+          <button type="submit" className="btn btn-outline-success d-flex align-items-center gap-2" disabled={disableButton.addButton || note.title.trim().length < 3 || note.description.trim().length < 5 ? true : false} onClick={handleOnAddClick}>
+            Add Note
+            {addApiCall && (
+              <div className="d-flex justify-content-center">
+                <div className="spinner-border spinner-border-sm" role="status">
+                  <span className="visually-hidden">Loading...</span>
+                </div>
+              </div>
+            )}
+          </button>
+          <button type="submit" className="btn btn-outline-success d-flex align-items-center gap-2 mx-3" disabled={disableButton.editButton || note.title.trim().length < 3 || note.description.trim().length < 5 ? true : false} onClick={handleOnEditClick}>
+            Edit Note
+            {editApiCall && (
+              <div className="d-flex justify-content-center">
+                <div className="spinner-border spinner-border-sm" role="status">
+                  <span className="visually-hidden">Loading...</span>
+                </div>
+              </div>
+            )}
+          </button>
+        </div>
       </form>
     </div>
   )
